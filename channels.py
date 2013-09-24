65bd1b29adf4d8d03a337d6aa6b03b4659029569
Matt Griffiths <mrmatthewgriffiths@gmail.com>
2013-09-23 20:34:46 -0400

    Added FB in sentances channel

--- channels.py
+++ channels.py
@@ -17,6 +17,8 @@ from django.utils.text import Truncator
 from django_odc.objects import ContentItemAuthor, ContentItem
 from django_odc.utils import format_error
 
+access_token = 'CAAESCkX9loYBAL0cxgiDUAADSYniLG2bTPxdnPPgdEhJkpmvcNZCW9L8v2h9SgwcxeB8ruxpbXzaTPCCBy9YZBpGZA76XXvJZCkZA6PwsxwmDakb9Iy2zaEoJyuZBBkOmF9AYZCZC9ZAeVw38GuUL6RlcfZAZCcDu3DGKEhMeBZA8n7ZCCzvZAZBcTIORxZB66J6UURG4lEZD'
+
 
 class _BaseChannel(object):
 
@@ -771,6 +773,213 @@ class FeedInToSentenceChannel(_BaseChannel):
         return content_items
 
 
+class FacebookPublicSearchIntoSentenceChannel(_BaseChannel):
+    _image_url_base = 'http://cdn1.iconfinder.com/data/icons/yooicons_set01_socialbookmarks'
+    _configuration = {
+        'type': 'facebook_public_search_into_sentence_v01',
+        'data_type': 'content_v01',
+        'aggregation_type': 'polling',
+        'images': {
+            '16': _image_url_base + '/16/social_facebook_box_blue.png',
+            '24': _image_url_base + '/24/social_facebook_box_blue.png',
+            '32': _image_url_base + '/32/social_facebook_box_blue.png',
+            '48': _image_url_base + '/48/social_facebook_box_blue.png',
+            '64': _image_url_base + '/64/social_facebook_box_blue.png',
+            '128': _image_url_base + '/128/social_facebook_box_blue.png'
+        },
+        'display_name_short': 'Facebook In Sentences',
+        'display_name_full': 'Public Facebook Posts Into Sentences',
+        'description_short': 'Collect public posts from Facebook in Sentences.',
+        'description_full': 'Use this source to search the public posts stream of Facebook',
+        'config': {
+            'elements': [
+                {
+                    'name': 'search',
+                    'display_name': 'Search Term(s)',
+                    'type': 'text',
+                    'help_message': 'Only numbers and characters separated by spaces (Note that all terms use '
+                                    'a logical AND).',
+                    'value': ''
+                },
+                {
+                    'name': 'limit',
+                    'display_name': 'Total Limit',
+                    'type': 'select',
+                    'help_message': 'Please choose the total number of content items that can be held for this '
+                                    'data source. Once this limit is reached, old items will drop off the bottom to '
+                                    'make way for new items.',
+                    'values': ['1000', '10000', '100000', '1000000'],
+                    'value': '10000',
+                }
+            ]
+        }
+    }
+
+    @classmethod
+    def ValidateAndReturnErrors(cls, configuration):
+        # Extract the elements from the config
+        elements = configuration['config']['elements']
+        # Get the search terms for checking
+        search_terms = [e for e in elements if e['name'] == 'search'][0]['value']
+        # If there are no search terms or they are not valid
+        if not search_terms or [c for c in search_terms if not c.isalnum() and c != ' ']:
+            # Return an error saying so
+            return ['The search term(s) you entered are not valid.']
+        # No errors = pass
+        return []
+
+    def run_test(self, source, configuration, test_result_id):
+        # Validate the config to ensure its ok - it should be but who knows :)
+        errors = FacebookPublicSearchChannel.ValidateAndReturnErrors(configuration)
+        #If these are any errors then set the test in error state
+        if errors:
+            # Build the status messages
+            status_messages = {'errors': ['This source is not correctly configured.'], 'infos': []}
+            # Update the test via the source object
+            return source.update_test_data(test_result_id, 'error', status_messages)
+        # Extract the elements from the config
+        elements = configuration['config']['elements']
+        # Get the search terms
+        search_terms = [e for e in elements if e['name'] == 'search'][0]['value']
+        # Get the fb items
+        try:
+            fb = facepy.GraphAPI(access_token)
+            results = fb.search(term=search_terms, type='post', limit=5)
+        except Exception, e:
+            # Format the error
+            error = format_error(e, sys.exc_info())
+            # If there is an error even getting the results
+            return source.update_test_data(
+                test_result_id,
+                'error',
+                {'errors': ['There was an error getting data from facebook.', error], 'infos': []})
+        # check that there are results
+        if not results or 'data' not in results or not results.get('data', None):
+            return source.update_test_data(
+                test_result_id,
+                'error',
+                {'errors': ['There were no results returned from facebook.'], 'infos': []})
+        # Array to hold the parsed content
+        parsed_results = []
+        for r in results.get('data', []):
+            try:
+                for parsed_item in self._parse_fb_item(r, source):
+                    if parsed_item.title:
+                        parsed_results.append(parsed_item)
+            except Exception, e:
+                # Format the error
+                error = format_error(e, sys.exc_info())
+                # If there is an error even getting the results
+                return source.update_test_data(
+                    test_result_id,
+                    'error',
+                    {'errors': ['Not all of the posts could be parsed.', error], 'infos': []})
+        # Update the source with the results and include any bozo errors as info messages
+        return source.update_test_data(
+            test_result_id,
+            'passed',
+            {'errors': [], 'infos': []},
+            parsed_results)
+
+    def run_polling_aggregation(self, source, configuration, run_record):
+        # Validate the config to ensure its ok - it should be but who knows :)
+        errors = FacebookPublicSearchChannel.ValidateAndReturnErrors(configuration)
+        #If these are any errors then set the test in error state
+        if errors:
+            # Build the status messages
+            status_messages = {'errors': ['This source is not correctly configured.'], 'infos': []}
+            # Update the test via the source object
+            return run_record.update('error', status_messages)
+        # Extract the elements from the config
+        elements = configuration['config']['elements']
+        # Get the search terms
+        search_terms = [e for e in elements if e['name'] == 'search'][0]['value']
+        # Get the fb items
+        try:
+            fb = facepy.GraphAPI(access_token)
+            raw_results = fb.search(term=search_terms, type='post', limit=100)
+        except Exception, e:
+            # Format the error
+            error = format_error(e, sys.exc_info())
+            # If there is an error even getting the results
+            return run_record.update(
+                'error',
+                {'errors': ['There was an error getting data from facebook.', error], 'infos': []})
+        # check that there are results
+        if not raw_results or 'data' not in raw_results or not raw_results.get('data', None):
+            return run_record.update(
+                'error',
+                {'errors': ['There were no results returned from facebook.'], 'infos': []})
+        # Array to hold the parsed content
+        results = []
+        for r in raw_results.get('data', []):
+            try:
+                for parsed_item in self._parse_fb_item(r, source):
+                    if parsed_item.title:
+                        results.append(parsed_item)
+            except Exception, e:
+                # Format the error
+                error = format_error(e, sys.exc_info())
+                # If there is an error even getting the results
+                return run_record.update(
+                    run_record.status,
+                    {'errors': ['Not all of the posts could be parsed.', error], 'infos': []})
+        # If there is a since time in the config
+        if 'since_time' in configuration:
+            try:
+                # Parse the since time as a datetime
+                since_time = datetime.datetime.fromtimestamp(configuration['since_time'])
+                # filter out all duplicates based on time created
+                results = [r for r in results if not r.created or r.created > since_time]
+            except:
+                pass
+        # calculate oldest and youngest
+        oldest = None
+        youngest = None
+        for r in results:
+            if r.created:
+                if not oldest or r.created < oldest:
+                    oldest = r.created
+                if not youngest or r.created > youngest:
+                    youngest = r.created
+        # Update the stats on the run record
+        run_record.record_statistics(total_item=len(results), oldest_datetime=oldest, youngest_datetime=youngest)
+        # update the run record
+        run_record.update('finished', {'errors': [], 'infos': []})
+        # Update the configuration with the new since_time
+        youngest_is_time = (youngest and isinstance(youngest, datetime.datetime))
+        configuration['since_time'] = time.mktime(youngest.timetuple()) if youngest_is_time else time.time()
+        # Return the results
+        return results
+
+    def _parse_fb_item(self, item, source):
+        created = None
+        item_id = item.get('id', None)
+        link_format = 'http://www.facebook.com/%s'
+        try:
+            timestamp = item.get('created_time')
+            created = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S+0000')
+        except Exception:
+            pass
+        author = ContentItemAuthor()
+        author.display_name = item.get('from', {}).get('name', None)
+        author.id = item.get('from', {}).get('id', None)
+        text = item.get('message', '')
+        sentences = re.split(r' *[\.\?!][\'"\)\]]* *', text)
+        content_items = []
+        for sentence in sentences:
+            content = ContentItem()
+            content.id = md5(item_id or ('%s' % (time.time() + randint(0, 1000)))).hexdigest()
+            content.source = source.to_dict()
+            content.author = author
+            content.title = sentence
+            if item_id:
+                content.link = link_format % (item_id.split('_')[1])
+            content.created = created
+            content_items.append(content)
+        return content_items
+
+
 class FacebookPublicSearchChannel(_BaseChannel):
     _image_url_base = 'http://cdn1.iconfinder.com/data/icons/yooicons_set01_socialbookmarks'
     _configuration = {
@@ -841,7 +1050,7 @@ class FacebookPublicSearchChannel(_BaseChannel):
         search_terms = [e for e in elements if e['name'] == 'search'][0]['value']
         # Get the fb items
         try:
-            fb = facepy.GraphAPI('CAAESCkX9loYBAL0cxgiDUAADSYniLG2bTPxdnPPgdEhJkpmvcNZCW9L8v2h9SgwcxeB8ruxpbXzaTPCCBy9YZBpGZA76XXvJZCkZA6PwsxwmDakb9Iy2zaEoJyuZBBkOmF9AYZCZC9ZAeVw38GuUL6RlcfZAZCcDu3DGKEhMeBZA8n7ZCCzvZAZBcTIORxZB66J6UURG4lEZD')
+            fb = facepy.GraphAPI(access_token)
             results = fb.search(term=search_terms, type='post', limit=5)
         except Exception, e:
             # Format the error
@@ -894,7 +1103,7 @@ class FacebookPublicSearchChannel(_BaseChannel):
         search_terms = [e for e in elements if e['name'] == 'search'][0]['value']
         # Get the fb items
         try:
-            fb = facepy.GraphAPI('CAAESCkX9loYBAL0cxgiDUAADSYniLG2bTPxdnPPgdEhJkpmvcNZCW9L8v2h9SgwcxeB8ruxpbXzaTPCCBy9YZBpGZA76XXvJZCkZA6PwsxwmDakb9Iy2zaEoJyuZBBkOmF9AYZCZC9ZAeVw38GuUL6RlcfZAZCcDu3DGKEhMeBZA8n7ZCCzvZAZBcTIORxZB66J6UURG4lEZD')
+            fb = facepy.GraphAPI(access_token)
             raw_results = fb.search(term=search_terms, type='post', limit=100)
         except Exception, e:
             # Format the error
